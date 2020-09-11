70816caa2c631ee7df7dcd56c2b5957c981c3493
Ayush Kulshrestha <akcool.ayush@gmail.com>
2020-09-12 00:45:14 +0530

    Updates

---
+++ precache-manifest.35b4cfbad917d17883740c494398a519.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10cfa2dd6abfb13bbbbd00d0618c3df6",
+    "url": "/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "ea646c78f6925e4a4065",
+    "url": "/react-expense-tracker/static/css/main.126d991b.chunk.css"
+  },
+  {
+    "revision": "ed41b672784623e20889",
+    "url": "/react-expense-tracker/static/js/2.9283fdf9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-expense-tracker/static/js/2.9283fdf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea646c78f6925e4a4065",
+    "url": "/react-expense-tracker/static/js/main.92c9f290.chunk.js"
+  },
+  {
+    "revision": "78c83d7ef0dc2332c961",
+    "url": "/react-expense-tracker/static/js/runtime-main.f8b53f9c.js"
+  },
+  {
+    "revision": "92f59c674930944dd79ac744b7110f6f",
+    "url": "/react-expense-tracker/static/media/plus.92f59c67.svg"
+  }
+]);
